d40575eb250854857b59ecdb7ea2fc2166bf41cb
aliluczak <aliluczak@gmail.com>
2017-10-02 21:54:29 +0200

    moving source with volume slider

---
+++ Projekt mag/Controller/SoundController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Audio.OpenAL;
+using OpenTK.Audio;
+using NAudio.Wave;
+using System.Media;
+using System.Threading;
+
+
+namespace Projekt_mag.Controller
+{
+    class SoundController
+    {
+        int _source;
+        int _buffer;
+        const string _fileName = "szum biały.wav";
+
+        public SoundController()
+        {
+           // _source = AL.GenSource();
+            //_buffer = AL.GenBuffer();
+           
+ //           SetParameters();
+        }
+
+        public void LoadAndPlay()
+        {
+
+            using (AudioContext audioContext = new AudioContext())
+            {
+                _source = AL.GenSource();
+                _buffer = AL.GenBuffer();
+                SetParameters();
+                KeyValuePair<byte[], int> audio = LoadSound();
+
+                AL.BufferData(_buffer, ALFormat.Mono16, audio.Key, audio.Key.Length* sizeof(byte), audio.Value);
+                
+                AL.Source(_source, ALSourcei.Buffer, _buffer);
+
+                SetPosition(0, 20, 2);
+                SetVolume(0.3f);
+                var initialState = AL.GetSourceState(_source);
+                AL.SourcePlay(_source);
+                var currentSourceState = ALSourceState.Playing;
+                while (!currentSourceState.Equals(ALSourceState.Stopped))
+                {
+                    Thread.Sleep(100);
+                    //MoveSource(0, -1, 0);
+                    currentSourceState = AL.GetSourceState(_source);
+                }
+
+                AL.DeleteBuffer(_buffer);
+                AL.DeleteSource(_source);
+            }
+        }
+
+        public void SetVolume(float volume)
+        {
+            AL.Source(_source, ALSourcef.Gain, volume);
+        }
+
+        public void SetPosition(float x, float y, float z)
+        {
+            AL.Source(_source, ALSource3f.Position, x, y, z);
+        }
+
+        public void SetVelocity(float x, float y, float z)
+        {
+            AL.Source(_source, ALSource3f.Velocity, x, y, z);
+        }
+
+        private void MoveSource(float x, float y, float z)
+        {
+            OpenTK.Vector3 currentPosition;
+            AL.GetSource(_source, ALSource3f.Position, out currentPosition);
+            AL.Source(_source, ALSource3f.Position, currentPosition.X + x, currentPosition.Y + y, currentPosition.Z + z);
+        }
+
+        #region Helpers
+        private KeyValuePair<byte[], int> LoadSound()
+        {
+          //  var waveOut = new WaveOutEvent();
+            WaveFileReader reader = new WaveFileReader(_fileName);
+            byte[] buffer = new byte[reader.Length];
+            int audioDataHandler = reader.Read(buffer, 0, buffer.Length);
+            //waveOut.Init(reader);
+            //waveOut.Play();
+            KeyValuePair<byte[], int> audiodata = new KeyValuePair<byte[], int>(buffer, reader.WaveFormat.SampleRate);
+            reader.Dispose();
+            //waveOut.Dispose();
+            return audiodata;
+        }
+
+        private void SetParameters()
+        {
+            AL.Listener(ALListener3f.Position, 0, 0, 0);
+            AL.Listener(ALListener3f.Velocity, 0, 0, 0);
+            AL.Source(_source, ALSource3f.Position, 0, 30, 0);
+            AL.Source(_source, ALSource3f.Velocity, 0, 0, 0);
+            AL.Source(_source, ALSource3f.Direction, 0, -1, 0);
+        }
+
+        #endregion
+    }
+}
--- Projekt mag/MainWindow.xaml.cs
+++ Projekt mag/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Projekt_mag.Controller;
 
 namespace Projekt_mag
 {
@@ -20,9 +21,22 @@ namespace Projekt_mag
     /// </summary>
     public partial class MainWindow : Window
     {
+        SoundController _controller;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            _controller = new SoundController();
+            //controller.TestSoundPlayer();
+            _controller.LoadAndPlay();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _controller.SetVolume((float)Volume.Value);
         }
     }
 }
